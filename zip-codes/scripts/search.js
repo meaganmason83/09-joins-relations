d6e820d3d9ab91cecc4888e66a74dbd08e52d66e
Meagan Mason <meagananne@Meagans-MacBook-Pro.local>
2016-12-08 20:19:17 -0800

    Working on search.js

--- zip-codes/scripts/search.js
+++ zip-codes/scripts/search.js
@@ -2,8 +2,23 @@
 
 (function(module) {
 
+  var searchView = {};
+
   // TODO: Write the code to populate your filters, and enable the search queries here in search.js
   // TODO: You will also interact with the map.js file here
+  searchView.populateFilters = function() {
+    webDB.execute('SELECT state FROM zips GROUP BY state;',
+    function(rows) {
+      if (rows.length) {
+        console.log(rows);
+      $('state-select').
+      } else {
+        $.getJSON('../../data/zips.json', function() {
 
+        });
+      }
+    });
+  };
 
-})(window)
+  searchView.populateFilters();
+})(window);
