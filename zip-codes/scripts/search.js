adc93e78e26b286c92b1453b3cf1171461cdc463
Meagan Mason <meagananne@Meagans-MacBook-Pro.local>
2016-12-10 10:30:47 -0800

    Got city function to filter by state

--- zip-codes/scripts/search.js
+++ zip-codes/scripts/search.js
@@ -6,25 +6,31 @@
 
   // TODO: Write the code to populate your filters, and enable the search queries here in search.js
   // TODO: You will also interact with the map.js file here
-  searchView.populateFilters = function() {
+  searchView.populateState = function() {
     webDB.execute('SELECT state FROM zips GROUP BY state;',
     function(rows) {
       if (rows.length) {
         rows.forEach(function(a) {
           $('#state-select').append('<option>' + a.state + '</option>');
         });
-        $('#state-select').on('change', function() {
-          webDB.execute('SELECT city FROM zips GROUP BY state;',
-          function(a) {
-            if ($('#city-select').text() === a.state) {
-              $('#city-select').append('<option>' + a.city + '</option>');
-            }
-          });
-        });
       }
     });
-
   };
 
-  searchView.populateFilters();
+  searchView.populateCity = function(state) {
+    $('#state-select').on('change', function() {
+      webDB.execute('SELECT DISTINCT city FROM zips WHERE state = "' + $(this).val() + '"',
+      function(a) {
+        a.forEach(function(ele) {
+          $('#city-select').append('<option>' + ele.city + '</option>');
+        });
+      }
+    );
+    });
+  };
+
+
+  searchView.populateState();
+  searchView.populateCity();
+
 })(window);
